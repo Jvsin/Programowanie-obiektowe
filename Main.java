c21d4fd866f4a675aa23e6846addb43e5273b16b
Jakub Tutka <97901705+JakubTuta@users.noreply.github.com>
2023-03-09 23:52:05 +0100

    Reads from file correctly

--- Main.java
+++ Main.java
@@ -6,26 +6,44 @@ import static java.lang.System.exit;
 
 public class Main {
     public static void main(String[] args) {
-        Map<String, String[]> mapa = new LinkedHashMap<>();
-        try {
-            File plik = new File("test.txt");
-            Scanner plikScanner = new Scanner(plik);
-            List wartosci = new ArrayList<String>();
-            while (plikScanner.hasNextLine()) {
-                String data = plikScanner.nextLine();
-                if(data.charAt(data.length() - 1) == ':') {
-                    String klucz = data;
-                }
-                else {
-                    wartosci.add(data);
-                }
+        File plik;
+        Scanner plikScanner = null;
 
-            }
-            plikScanner.close();
+        try {
+            plik = new File("test.txt");
+            plikScanner = new Scanner(plik);
         } catch (FileNotFoundException e) {
             System.out.println("Nie znaleziono pliku.");
             e.printStackTrace();
             exit(1);
+        }
+
+        List<String> wartosci = new ArrayList<>();
+        Map<String, List<String>> mapa = new LinkedHashMap<>();
+        String klucz = null;
+        boolean pierwsze = true;
+
+        while (plikScanner.hasNextLine()) {
+            String data = plikScanner.nextLine();
+            if (data.endsWith(":")) {
+                if (!pierwsze) {
+                    mapa.put(klucz, wartosci);
+                    wartosci = new ArrayList<>();
+                }
+                klucz = data;
+                pierwsze = false;
+            } else {
+                wartosci.add(data);
+            }
+        }
+        plikScanner.close();
+        mapa.put(klucz, wartosci);
+
+        for (Map.Entry<String, List<String>> dane : mapa.entrySet()) {
+            System.out.println(dane.getKey());
+            for (String wartosc : dane.getValue()) {
+                System.out.println("\t" + wartosc);
+            }
         }
     }
 }
