8ee8ba5d85c309150e986ee329b003c79f011030
Jakub Tutka <97901705+JakubTuta@users.noreply.github.com>
2023-03-10 18:28:05 +0100

    działa

--- Main.java
+++ Main.java
@@ -1,5 +1,7 @@
 import java.io.File;
 import java.io.FileNotFoundException;
+import java.io.FileWriter;
+import java.io.IOException;
 import java.util.*;
 
 import static java.lang.System.exit;
@@ -20,8 +22,9 @@ public class Main {
 
         List<String> wartosci = new ArrayList<>();
         Map<String, List<String>> mapa = new LinkedHashMap<>();
-        String klucz = null;
-        boolean pierwsze = true;
+        String wybor, nazwa, klucz = null, przedmiot;
+        boolean pierwsze = true, wyjdz = false;
+        Scanner scanner = new Scanner(System.in);
 
         while (plikScanner.hasNextLine()) {
             String data = plikScanner.nextLine();
@@ -39,10 +42,178 @@ public class Main {
         plikScanner.close();
         mapa.put(klucz, wartosci);
 
-        for (Map.Entry<String, List<String>> dane : mapa.entrySet()) {
-            System.out.println(dane.getKey());
-            for (String wartosc : dane.getValue()) {
-                System.out.println("\t" + wartosc);
+        while(!wyjdz) {
+            System.out.println("wyjdź");
+            System.out.println("wyświetl wszystko");
+            System.out.println("wyświetl kategorie");
+            System.out.println("dodaj kategorie");
+            System.out.println("dodaj produkty");
+            System.out.println("usuń wszystko");
+            System.out.println("usuń kategorie");
+            System.out.println("usuń produkt");
+            System.out.println();
+            System.out.println("Co chcesz zrobić:");
+
+            wybor = scanner.nextLine();
+            System.out.println();
+
+            switch(wybor) {
+                case "wyjdź":
+                    wyjdz = true;
+                    break;
+
+                case "wyświetl wszystko":
+                    for (Map.Entry<String, List<String>> dane : mapa.entrySet()) {
+                        System.out.println(dane.getKey());
+
+                        if(dane.getValue() == null) {
+                            System.out.println("\tPusto");
+                            continue;
+                        }
+
+                        for (String wartosc : dane.getValue()) {
+                            System.out.println("\t" + wartosc);
+                        }
+                    }
+                    System.out.println();
+                    break;
+
+                case "wyświetl kategorie":
+                    for (Map.Entry<String, List<String>> dane : mapa.entrySet()) {
+                        klucz = dane.getKey();
+                        System.out.println(klucz.substring(0, klucz.length() - 1));
+                    }
+                    System.out.println();
+                    System.out.println("Podaj kategorię, którą chcesz wyświetlić:");
+                    nazwa = scanner.nextLine() + ":";
+                    if(!mapa.containsKey(nazwa)) {
+                        System.out.println("Nie istnieje taka kategoria");
+                        break;
+                    }
+
+                    wartosci = mapa.get(nazwa);
+                    for(String produkt : wartosci) {
+                        System.out.println("\t" + produkt);
+                    }
+                    System.out.println();
+                    break;
+
+                case "dodaj kategorie":
+                    System.out.print("Podaj nazwę kategorii: ");
+                    nazwa = scanner.nextLine() + ":";
+                    mapa.put(nazwa, null);
+                    System.out.println();
+                    break;
+
+                case "dodaj produkty":
+                    System.out.println("Podaj nazwę kategorii, do której chcesz dodać produkty: ");
+                    for (Map.Entry<String, List<String>> dane : mapa.entrySet()) {
+                        klucz = dane.getKey();
+                        System.out.println(klucz.substring(0, klucz.length() - 1));
+                    }
+                    System.out.println();
+
+                    nazwa = scanner.nextLine() + ":";
+                    if(!mapa.containsKey(nazwa)) {
+                        System.out.println("Nie istnieje taka kategoria");
+                        break;
+                    }
+
+                    System.out.println("Podaj produkty do dodania:");
+                    wartosci = mapa.get(nazwa);
+                    if(wartosci == null) {
+                        wartosci = new ArrayList<>();
+                    }
+                    while(scanner.hasNextLine()) {
+                        przedmiot = scanner.nextLine();
+                        if(przedmiot.isEmpty()) {
+                            break;
+                        }
+
+                        if(!wartosci.contains(przedmiot)) {
+                            wartosci.add(przedmiot);
+                        }
+                    }
+
+                    mapa.put(nazwa, wartosci);
+                    System.out.println();
+                    break;
+
+                case "usuń wszystko":
+                    mapa = new LinkedHashMap<>();
+                    System.out.println();
+                    break;
+
+                case "usuń kategorie":
+                    System.out.println("Podaj kategorię, którą chcesz usunąć:");
+                    nazwa = scanner.nextLine() + ":";
+                    if(!mapa.containsKey(nazwa)) {
+                        System.out.println("Nie istnieje taka kategoria");
+                        break;
+                    }
+                    mapa.remove(nazwa);
+                    break;
+
+                case "usuń produkt":
+                    System.out.println("Podaj nazwę kategorii, do której chcesz usunąć produkty: ");
+                    for (Map.Entry<String, List<String>> dane : mapa.entrySet()) {
+                        klucz = dane.getKey();
+                        System.out.println(klucz.substring(0, klucz.length() - 1));
+                    }
+                    System.out.println();
+
+                    nazwa = scanner.nextLine() + ":";
+                    if(!mapa.containsKey(nazwa)) {
+                        System.out.println("Nie istnieje taka kategoria");
+                        break;
+                    }
+
+                    wartosci = mapa.get(nazwa);
+                    for(String produkt : wartosci) {
+                        System.out.println("\t" + produkt);
+                    }
+
+                    System.out.println("Podaj produkty do usunięcia:");
+                    wartosci = mapa.get(nazwa);
+                    while(scanner.hasNextLine()) {
+                        przedmiot = scanner.nextLine();
+                        if(przedmiot.isEmpty()) {
+                            break;
+                        }
+
+                        if(wartosci.contains(przedmiot)) {
+                            wartosci.remove(przedmiot);
+                        }
+                    }
+                    System.out.println();
+                    break;
+
+                default:
+                    System.out.println("Niepoprawny wybór");
+                    break;
+            }
+        }
+
+        System.out.println("Czy chcesz zapisać te liste? (tak/nie)");
+        wybor = scanner.nextLine();
+        if(wybor.equals("tak")) {
+            System.out.print("Podaj nazwę pliku: ");
+            nazwa = scanner.nextLine();
+
+            try {
+                FileWriter writer = new FileWriter(nazwa);
+
+                for (Map.Entry<String, List<String>> dane : mapa.entrySet()) {
+                    writer.write(dane.getKey() + "\n");
+                    for (String wartosc : dane.getValue()) {
+                        writer.write(wartosc + "\n");
+                    }
+                }
+                writer.close();
+                System.out.println("Zapisano");
+            } catch (IOException e) {
+                System.out.println("Nie udało się otworzyć pliku");
+                e.printStackTrace();
             }
         }
     }
