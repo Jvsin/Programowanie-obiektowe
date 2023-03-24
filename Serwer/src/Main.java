e63ce63352defe3efce0a148c5eb5fdebcda8e36
Jakub Tutka <97901705+JakubTuta@users.noreply.github.com>
2023-03-24 13:36:24 +0100

    Fixed server-side

--- Serwer/src/Main.java
+++ Serwer/src/Main.java
@@ -1,30 +1,51 @@
 import java.io.*;
 import java.net.*;
-import static java.lang.System.exit;
+import java.util.ArrayList;
+import java.util.Scanner;
+import java.util.concurrent.ExecutorService;
+import java.util.concurrent.Executors;
 
 public class Main {
     public static void main(String[] args) {
         ServerSocket serverSocket = null;
+        Scanner scanner = new Scanner(System.in);
+        ExecutorService executor = Executors.newFixedThreadPool(10);
+        ArrayList<Socket> klienci = new ArrayList<>();
 
         try {
-            serverSocket = new ServerSocket(4444);
+            serverSocket = new ServerSocket(2137);
         } catch (IOException e) {
             System.err.println("Nie udało się utworzyć hosta");
-            exit(1);
+            System.exit(1);
         }
         System.out.println("Utworzono hosta");
 
-        while (true) {
-            Socket socket;
-            try {
-                socket = serverSocket.accept();
-                System.out.println("Połączono z klientem: " + socket.getInetAddress().getHostAddress());
-            } catch (IOException e) {
-                System.err.println("Nie udało się połączyć");
-                continue;
+        final ServerSocket finalServerSocket = serverSocket;
+        new Thread(() -> {
+            while (true) {
+                Socket clientSocket;
+                try {
+                    clientSocket = finalServerSocket.accept();
+                    System.out.println("Połączono z klientem: " + clientSocket.getInetAddress().getHostAddress());
+                    klienci.add(clientSocket);
+                } catch (IOException e) {
+                    System.err.println("Nie udało się połączyć");
+                }
             }
+        }).start();
 
-            new Thread(new KlientHandler(socket)).start();
-        }
+        new Thread(() -> {
+            while (true) {
+                System.out.println("Podaj tekst");
+                String text = scanner.nextLine();
+                for (Socket klient : klienci) {
+                    executor.submit(new KlientHandler(klient, text));
+                }
+                if(text.isEmpty()) {
+                    break;
+                }
+            }
+            executor.shutdown();
+        }).start();
     }
 }
